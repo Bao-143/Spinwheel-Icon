39fd6e28222af100ea007def956c6ab01633e6d4
Bao143 <taodiepgiabao070414@gmail.com>
2026-06-02 18:45:58 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,9 @@ class $modify(SpinPlayer, PlayerObject) {
     struct Fields {
         float spinOffset = 0.f;
     };
-
     void update(float dt) {
         PlayerObject::update(dt);
-
         m_fields->spinOffset += 360.f * dt;
-
         if (m_iconSprite) {
             m_iconSprite->setRotation(
                 m_iconSprite->getRotation() + m_fields->spinOffset
