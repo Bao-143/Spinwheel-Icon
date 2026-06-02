894d70e50e08e1a5f7be66d5189a19bc89227a87
Bao143 <taodiepgiabao070414@gmail.com>
2026-06-02 15:39:14 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/PlayerObject.hpp>
+#include <Geode/modify/GJBaseGameLayer.hpp>
 
 using namespace geode::prelude;
 
-class $modify(SpinPlayer, PlayerObject) {
-    void update(float dt) {
-        PlayerObject::update(dt);
+class $modify(SpinPlayer, GJBaseGameLayer) {
+    void processCommands(float dt) {
+        GJBaseGameLayer::processCommands(dt);
 
-        if (auto icon = m_iconSprite) {
-            icon->setRotation(icon->getRotation() + 360.f * dt);
+        auto player1 = m_player1;
+        auto player2 = m_player2;
+
+        if (player1) {
+            player1->setRotation(player1->getRotation() + 360.f * dt);
+        }
+
+        if (player2) {
+            player2->setRotation(player2->getRotation() + 360.f * dt);
         }
     }
 };
